cfdc28b5bfc84a8e1b569c4c52befce6af7efdea
mintu321-prog <islammintu454@gmail.com>
2026-07-28 12:02:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const message = "Every moment with you is my favourite memory. Happy Anniversary DOLA ❤️";
+const text =
+"Every love story is beautiful, but ours is my favorite. ❤️";
 
 let i = 0;
+const typing = document.getElementById("typing");
 
-function typeWriter() {
-    if (i < message.length) {
-        document.querySelector("p").innerHTML += message.charAt(i);
+function typeEffect() {
+    if (i < text.length) {
+        typing.innerHTML += text.charAt(i);
         i++;
-        setTimeout(typeWriter, 50);
+        setTimeout(typeEffect, 50);
     }
 }
 
-document.querySelector("p").innerHTML = "";
-typeWriter();
+typeEffect();
+
+const btn = document.getElementById("openBtn");
+const hero = document.querySelector(".hero");
+const letter = document.getElementById("letter");
+
+btn.onclick = function () {
+    hero.style.display = "none";
+    letter.style.display = "block";
+};
+
+const images = [
+    "images/pic1.jpg",
+    "images/pic2.jpg"
+];
+
+let current = 0;
+const slider = document.getElementById("slider");
+
+setInterval(() => {
+    current++;
+    if (current >= images.length) current = 0;
+    slider.src = images[current];
+}, 3000);
+
+const startDate = new Date("September 21, 2022");
+
+function updateCounter() {
+    const now = new Date();
+    const diff = now - startDate;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    document.getElementById("counter").innerHTML =
+        "❤️ Together for <b>" + days + "</b> Days ❤️";
+}
+
+updateCounter();
+setInterval(updateCounter, 1000);
